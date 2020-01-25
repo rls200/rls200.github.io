b7facaf51471c5bc0f20ed98938135120fd6dcac
razhihan11 <razhihan11@gmail.com>
2020-01-25 19:21:59 +0300

    build

---
+++ precache-manifest.bf16a2cbefbbaedf13fd9370cdcf7bbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9641b876d7fd254e6108a859c7fecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c71a9a76d9c79512f74d",
+    "url": "/static/css/2.d12ee2e6.chunk.css"
+  },
+  {
+    "revision": "b9fd8fd672a230404d17",
+    "url": "/static/css/main.96e2fff5.chunk.css"
+  },
+  {
+    "revision": "c71a9a76d9c79512f74d",
+    "url": "/static/js/2.5e1d206b.chunk.js"
+  },
+  {
+    "revision": "ab877219b1921c5db1e8133969735e1d",
+    "url": "/static/js/2.5e1d206b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9fd8fd672a230404d17",
+    "url": "/static/js/main.b0acbe69.chunk.js"
+  },
+  {
+    "revision": "4da56af30bd4e0eb08f5",
+    "url": "/static/js/runtime-main.8560f125.js"
+  }
+]);
